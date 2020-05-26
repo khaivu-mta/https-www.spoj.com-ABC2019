68691125987a186941c494cad4cfa697645aac9a
Khải Vũ <44091521+cullen2205@users.noreply.github.com>
2020-05-26 12:25:35 +0700

    Update EASUDOKU.cpp

--- EZSUDOKU - Easy modified sudoku/EASUDOKU.cpp
+++ EZSUDOKU - Easy modified sudoku/EASUDOKU.cpp
@@ -1,139 +1,114 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
- 
-// Lưu ma trận suduku
-int Matrix[9][9];               
+int matrix[10][10];
+int flag;
 
-// Kết quả bài toán, Answer = true => có giải pháp, Answer = false nghĩa là không có
-bool Answer;                
-
-// Lưu trạng thái tại từng hàng, cột từ 0 đến 9, các số từ 1 đến 9 đã có chưa    
-bool Row[9][10], Col[9][10];
- 
-void Next(int &row, int &col)
+int check_blocks(int x, int y)
 {
-    if(col < 8) col++;
-    else 
-    {
-        col = 0;
-        row++;
+    int startx = x/4;
+    int starty = y/4; int end_x = startx*4 + 4; int end_y = starty*4 + 4;
+    int F[9];
+    for(int ii=0; ii<9; ii++)F[ii] = 0;
+    for(int i=startx*4; i<end_x; i++)
+        for(int j=starty*4; j<end_y; j++)
+        {
+            if(matrix[i][j] == 0)continue;
+            F[matrix[i][j]]++;
+            if(F[matrix[i][j]]>2)
+                return 1;
+        }
+        return 0;
+}
+int check_row_col(int index_x, int index_y)
+{
+    int F_row[10];
+    int F_col[10];
+    for(int i=0; i<9; i++)F_row[i] = F_col[i] =0;
+    // hang
+    for(int i=0; i<8; i++){
+        if(matrix[index_x][i]==0)continue;
+        F_row[matrix[index_x][i]]++;
+        if(F_row[matrix[index_x][i]]>1)
+            return 1;
     }
+    // cot
+    for(int i=0; i<8; i++){
+        if(matrix[i][index_y]==0)continue;
+        F_col[matrix[i][index_y]]++;
+        if(F_col[matrix[i][index_y]]>1)
+            return 1;
+    }
+    return 0;
 }
- 
-bool IsValid(int row, int col, int value)
+int check_full_row_col()
 {
-    // Kiểm tra xem tại cột đang xét, giá trị 'value' này đã có chưa
-    if(Col[col][value] == true) return false;
- 
-    // Kiểm tra xem tại hàng đang xét, giá trị 'value' này đã có chưa
-    if(Row[row][value] == true) return false;
- 
-    // Kiểm tra xem trong khối nhỏ 3x3 đã có giá trị 'value' hay chưa
-    int sr = row - row % 3;
-    int sc = col - col % 3;
- 
-    for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 3; j++)
-            if(Matrix[i + sr][j + sc] == value) return false;
- 
-    return true;
+    for(int i=0; i<8; i++)
+        if(check_row_col(i,i)==1)return 1;
+    return 0;
 }
- 
-void Check(int row, int col)
+void sodoku(int x, int y)
 {
-    // Nếu đi được hết các dòng thì có nghĩa là đã có giải pháp
-    if(row == 9)
+    if(x>=8)
     {
-        Answer = true;
+        flag = 1;
         return;
     }
- 
-    // Nếu tại ô đó đã có giá trị khác 0 thì ta sẽ kiểm tra đến ô tiếp theo
-    if (Matrix[row][col] != 0) 
+    if(matrix[x][y]!=0)
     {
-        Next(row, col);
-        Check(row, col);
+        if(y<7)sodoku(x, y+1);
+        else sodoku(x+1, 0);
     }
-    else // Nếu ô đó = 0 thì bắt đầu điền thử giá trị từ 1 đến 9
+    else
     {
-        int old_row, old_col;
- 
-        for(int i = 1; i <= 9; i++)
+        for(int i=1; i<=8; i++)
         {
-            // Kiểm tra xem nếu ô tại hàng row, và cột col, 
-            // điền giá trị i có hợp lệ không. Nếu hợp lệ thì điền thử
-            if(IsValid(row, col, i))
+            matrix[x][y] = i;// dat thu? gia tri i vao o toa do x, y
+            if(!check_row_col(x, y)&&!check_blocks(x, y))
             {
-                old_row = row;
-                old_col = col;
-        
-                Matrix[row][col] = i;
-                Row[row][i] = true;
-                Col[col][i] = true;
-                Next(row,col);
- 
-                Check(row,col);
- 
-                if(Answer) return;
- 
-                row = old_row;
-                col = old_col;
-                Row[row][i] = false;
-                Col[col][i] = false;
-                Matrix[row][col] = 0;
+                if(y<7)sodoku(x, y+1);
+                else sodoku(x+1, 0);                
             }
+            if(flag==1)return;
+            matrix[x][y] = 0;
         }
     }
 }
- 
+
 int main()
 {
-    ios::sync_with_stdio(false);
- 
-    // Comment dòng này trước khi submit
-    // freopen("input.txt","r",stdin);
- 
-    int T;
-    cin >> T;
- 
-    for(int tc = 0; tc < T; tc++)
+    int nTestCase;
+    scanf("%d", &nTestCase);
+    for(int test=1; test<=nTestCase; test++)
     {
-        Answer = false;
-        for(int i = 0; i < 9; i++)
-            for(int j = 1; j <= 9; j++)
-                Row[i][j] = Col[i][j] = 0;
- 
-        // Nhập vào ma trận
-        // đồng thời kiểm lưu trạng thái các số đã có hay chưa tại từng hàng, cột
-        for(int i = 0; i < 9; i++)
-            for(int j = 0; j < 9; j++)
+        int startx = -1, starty = -1;
+        for(int i=0; i<8; i++)
+            for(int j=0; j<8; j++)
             {
-                int tmp;
-                cin >> tmp;
-                Matrix[i][j] = tmp;
- 
-                // Tại dòng i, số có giá trị tmp đã xuất hiện
-                Row[i][tmp] = true;
-                // Tại cột j, số có giá tị tmp đã xuất hiện
-                Col[j][tmp] = true;
+                scanf("%d", &matrix[i][j]);
+                if(matrix[i][j]==0&&startx==-1)
+                {
+                    startx = i;
+                    starty = j;
+                }
             }
- 
-        // Bắt đầu xét từ ô đầu tiên hàng = 0 và cột = 0
-        Check(0, 0);
- 
-        // In kết quả
-        if(Answer)
-        {
-            for(int i = 0; i < 9; i++)
+            flag = 0;
+            if(!check_blocks(0,0)&&!check_blocks(0,4)&&!check_blocks(4,0)&&!check_blocks(4,4)&&!check_full_row_col())
             {
-                for(int j = 0; j < 9; j++)
-                    cout << Matrix[i][j] << " ";
-                cout << endl;
+                sodoku(startx, starty);
+                if(flag)
+                {
+                    printf("Test case #%d: YES\n", test);
+                    for(int i=0; i<8; i++)
+                    {
+                        for(int j=0; j<8; j++)
+                            printf("%d ", matrix[i][j]);
+                        printf("\n");
+                    }
+                }
+                else printf("Test case #%d: NO\n", test);
             }
-        }
-        else cout << "No solution" << endl;
-        
+            else printf("Test case #%d: NO\n", test);
     }
- 
     return 0;
 }
